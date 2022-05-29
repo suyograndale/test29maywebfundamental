4c05071b300f90458b41b1601b3205f5aeab0a51
suyograndale <78803822+suyograndale@users.noreply.github.com>
2022-05-29 22:15:52 +0545

    Add files via upload

---
+++ que3.js
@@ -0,0 +1,9 @@
+let=('Enter the number: len= ,bre= ');
+
+if (len=' ',bre=' ')
+{
+    console.log('rectangle');
+}
+else{
+    console.log('square');
+}
---
+++ que5.js
@@ -0,0 +1,26 @@
+switch (new Date().getDay()) {
+    case 0:
+        day = "Saturday";
+        break;
+    case 1:
+        day= "Sunday";
+        break;
+    case 2:
+        day = "Monday";
+        break;
+    case 3:
+        day = "Monday";
+        break;
+    case 4:
+        day = "Tuesday";
+        break;
+    case 5:
+        day = "Wednesday";
+        break;
+    case 6:
+        day = "Thursday";
+        break;
+    case 7:
+        day = "Friday";
+        break;
+}
